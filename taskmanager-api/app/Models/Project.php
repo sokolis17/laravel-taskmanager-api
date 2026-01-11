2dc7017116eb41ca8cbfd4ba4fb702b45353d86b
Igor <rononi@bk.ru>
2026-01-11 12:49:48 +0300

    Создал миграции,актуальную структуру бд и установил между ними связи

---
+++ taskmanager-api/app/Models/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'user_id','title','description','status'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
+}
---
+++ taskmanager-api/app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'project_id','title','description','status','priority','due_date'
+    ];
+
+    public function project(){
+       return $this->belongsTo(Project::class);
+    }
+
+}
