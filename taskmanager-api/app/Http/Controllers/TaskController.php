7d72254992081b109d59c5d61c9974dfbc053d15
Igor <rononi@bk.ru>
2026-01-13 20:25:17 +0300

    Реализовал роуты и логику TaskController !

---
+++ taskmanager-api/app/Http/Controllers/TaskController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'project_id' => 'required|exists:projects,id',
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:pending,completed',
+            'priority' => 'required|in:low,medium,high',
+            'due_date' => 'nullable|date'
+        ]);
+
+        $task = Task::create($validated);
+
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Task::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $validated = $request->validate([
+            'project_id' => 'sometimes|exists:projects,id',
+            'title' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'sometimes|in:pending,completed',
+            'priority' => 'sometimes|in:low,medium,high',
+            'due_date' => 'nullable|date'
+        ]);
+
+        $task->update($validated);
+
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(null,204);
+    }
+}
--- taskmanager-api/routes/api.php
+++ taskmanager-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('projects',ProjectController::class);
+Route::apiResource('tasks',TaskController::class);
